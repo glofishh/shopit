299725d3b05b5c02b97fdc2bda01e0f7ca8a9042
glofishh <gloriaulee@gmail.com>
2019-08-04 22:54:57 -0400

    refactored favorites after testing front-end ... whew

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -1,85 +1,46 @@
-const { FavoriteItem, FavoritesList } = require('../models/favorites');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 const Product = require('../models/product');
 const User = require("../models/user");
 
-
-exports.favoriteItemById = (req, res, next, id) => {
-  FavoriteItem.findById(id).exec((err, favorite) => {
-      if (err || !favorite) {
-          console.log(id)
-          return res.status(400).json({
-              error: "Favorite item not found"
-          });
-      }
-      req.favorite = favorite;
-      next();
-  });
-};
 
 exports.add = (req, res) => {
   // console.log(req.body)
   req.body.user = req.profile;
   // console.log('USER ' + req.body.user)
 
-  const favorite = new FavoriteItem(req.body);
-  favorite.save((error, data) => {
-    if (error) {
-      return res.status(400).json({
-        error: errorHandler(error)
-      });
-    }
-    User.findOneAndUpdate(
-      { _id: req.body.user._id },
-      { $addToSet: { favorites: favorite } },
-      ((error, result) => {
-        if (error) {
-          return res.status(400).json({
-            error: 'could not update user favorites'
-          });
-        }
-      })
-    )
-    res.json(`user ${req.profile.name} favorites list updated with ${req.body.name}`)
-  });
+  const favorite = Product(req.body);
+  User.findOneAndUpdate(
+    { _id: req.body.user._id },
+    { $addToSet: { favorites: favorite } },
+    ((error, result) => {
+      if (error) {
+        return res.status(400).json({
+          error: 'could not update user favorites'
+        })
+      }
+      res.json('added to favorites')
+    })
+  )
 };
 
 exports.remove = (req, res) => {
-  // console.log(req.params)
-  req.params.userId = req.profile;
+  console.log(req.body)
+  req.body.user = req.profile;
+  const removedFavorite = Product(req.body);
 
-  FavoriteItem.find(
-    { _id: req.params.favoriteItemId },
-    (error, item) => {
+  User.findOneAndUpdate(
+    { _id: req.body.user._id },
+    { $pull: { favorites: removedFavorite } },
+    ((error, result) => {
       if (error) {
         return res.status(400).json({
-          error: `couldn't find item matching params`
+          error: 'could not update user favorites'
         })
       }
-      User.findOneAndUpdate(
-        { _id: req.params.userId },
-        { $pull: { favorites: { $in: item } } },
-        { multi: true },
-        (error, success) => {
-          if (error) {
-            return res.status(400).json({
-              error: `could not update user favorites`
-            });
-          }
-          // console.log('found user and updated ' + success)
-        }
-      )
-    }  
-  ).remove((err, result) => {
-    if (err) {
-      return res.status(400).json({
-        error: errorHandler(err)
-      });
-    }
-    res.json(result)
-  })
+      res.json(`deleted ${removedFavorite.name}`)
+    })
+  )
 };
-
 
 exports.clear = (req, res) => {
   console.log(req.params)
--- routes/favorites.js
+++ routes/favorites.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { favoriteItemById, add, remove, clear } = require('../controllers/favorites');
-const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const { add, remove, clear } = require('../controllers/favorites');
+const { requireSignin, isAuth } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 const { productById } = require('../controllers/product');
 
 
 
 router.post('/favorites/add/:userId', requireSignin, isAuth, add);
-router.put('/favorites/remove/:favoriteItemId/:userId', requireSignin, isAuth, remove);
-router.put('/favorites/clear/all/:userId', requireSignin, isAuth, clear);
+router.put('/favorites/remove/:userId', requireSignin, isAuth, remove);
 
-router.param('favoriteItemId', favoriteItemById);
+router.param('productId', productById);
 router.param('userId', userById);
 
 
