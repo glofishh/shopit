4ffc9b51159498fb46e849c29b0fb192f09fb606
glofishh <gloriaulee@gmail.com>
2019-08-14 19:32:20 -0400

    prepare to deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -17,13 +18,14 @@ const favoritesRoutes = require('./routes/favorites');
 
 //app
 const app = express();
+const port = process.env.PORT || 8000
 
 //db
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true
   })
-  .then(() => console.log('DB connected!'));
+  .then(() => console.log('DB connected to Atlas!'));
 // mongoose
 //   .connect(process.env.DATABASE, {
 //     useNewUrlParser: true,
@@ -42,6 +44,30 @@ app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
 
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+//production mode
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname, 'build', 'index.html'));
+  });
+};
+
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));})
+
+//start server
+app.listen(port, () => {
+  console.log( `server is listening on port ${port}`);
+})
+
+
+
+
 //routes middleware
 //use /api to get all routes but in localhost add /api to url
 app.use('/api', authRoutes);
@@ -52,8 +78,8 @@ app.use('/api', braintreeRoutes);
 app.use('/api', orderRoutes);
 app.use('/api', favoritesRoutes);
 
-const port = process.env.PORT || 8000
 
-app.listen(port, () => {
-  console.log(`server is listening on ${port}`);
-});
+
+// app.listen(port, () => {
+//   console.log(`server is listening on ${port}`);
+// });
