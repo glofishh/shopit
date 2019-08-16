86185c8a2c0eced0f23c0b653953bebbec6b5cc8
glofishh <gloriaulee@gmail.com>
2019-08-16 10:22:48 -0400

    playing around with mongo .env variables ...... ._.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 
 //db
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true
   })
   .then(() => console.log('DB connected!'));
